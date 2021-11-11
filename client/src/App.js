95c6fcf951831660bd1fb52d604493606adef283
Benedict Schurwanz <benedictify@gmail.com>
2021-11-10 22:19:54 -0600

    implement delete movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,9 @@ const App = (props) => {
 			});
 	}, []);
 
-	const deleteMovie = (id) => {
+	const deleteMovie = (idParam) => {
+		const list = movies.filter(movie => movie.id != idParam)
+		setMovies(list)
 	}
 
 	const addToFavorites = (movie) => {
@@ -48,7 +50,7 @@ const App = (props) => {
 						</Route>
 
 						<Route path="/movies/:id">
-							<Movie />
+							<Movie deleteMovie={deleteMovie}/>
 						</Route>
 
 						<Route path="/movies">
